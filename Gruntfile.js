2ca3a089d8bbfaa5359c06aca4d871054a869aea
David Braden <neonphog@neonphog.com>
2015-03-17 16:29:00 -0600

    add build task / version handling

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,7 @@
+var fs = require('fs');
+var UglifyJS = require('uglify-js');
+var semver = require('semver');
+
 module.exports = function (grunt) {
   grunt.initConfig({
     jscs: {
@@ -33,5 +37,59 @@ module.exports = function (grunt) {
   grunt.registerTask('test', ['jscs', 'jshint', 'mochaTest']);
 
   grunt.registerTask('default', ['test']);
+
+  function minify(outfile, infiles) {
+    var min = UglifyJS.minify(infiles, {
+      output: {
+        "ascii_only": true
+      }
+    }).code;
+
+    fs.writeFileSync(outfile, min);
+    console.log('Generated "' + outfile + '" (' + min.length + ' bytes)');
+  }
+
+  function bump(version) {
+    var common = fs.readFileSync('lib/common.js').toString();
+    var sidx = common.indexOf("VERSION:START");
+    sidx = common.indexOf("\n", sidx);
+    var eidx = common.indexOf("VERSION:END");
+    eidx = common.lastIndexOf("\n", eidx);
+
+    if (!version) {
+      version = JSON.parse("{" + common.substring(sidx, eidx) + "}").version;
+      var pre = semver.parse(version).prerelease[0] || 'alpha';
+      version = semver.inc(version, 'prerelease', pre);
+    }
+    var newCommon = common.substring(0, sidx);
+    newCommon += '\n    "version": "' + version + '"';
+    newCommon += common.substring(eidx);
+    fs.writeFileSync('lib/common.js', newCommon);
+
+    var pack = JSON.parse(fs.readFileSync('package.json'));
+    pack.version = version;
+    fs.writeFileSync('package.json', JSON.stringify(pack, null, '  '));
+  }
+
+  grunt.registerTask('build', function (version) {
+    bump(version);
+
+    minify('browser/smjs.acorn.full.min.js', [
+      'node_modules/acorn/acorn.js',
+      'lib/common.js',
+      'lib/encoder.js',
+      'lib/decoder.js'
+      ]);
+    minify('browser/smjs.full.min.js', [
+      'lib/common.js',
+      'lib/encoder.js',
+      'lib/decoder.js'
+      ]);
+    minify('browser/smjs.decoder.min.js', [
+      'lib/common.js',
+      'lib/decoder.js'
+      ]);
+  });
+
 };
 
